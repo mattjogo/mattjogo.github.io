3c8bf535d64e9c1d1d5759cd4f319b99efbb5cd7
Mattjo <highbrighten@gmail.com>
2019-11-28 11:25:07 +0900

    시작 시간 변경

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 $('document').ready(function() {
-	var options = { videoId: 'e6y5JpPG0Ug', start: 39, repeat: true, mute: false };
+	var options = { videoId: 'e6y5JpPG0Ug', start: 0, repeat: true, mute: false };
 	$('#wrapper').tubular(options);
 	// f-UGhWj1xww cool sepia hd
 	// 49SKbS7Xwf4 beautiful barn sepia

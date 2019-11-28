6bc2e486597a6755a3b78de4b2275b1f2134e47e
Mattjo <highbrighten@gmail.com>
2019-11-28 09:47:02 +0900

    navigator 수정

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 $('document').ready(function() {
-	var options = { videoId: '49SKbS7Xwf4', start: 0, repeat: true, mute: true };
+	var options = { videoId: 'e6y5JpPG0Ug', start: 0, repeat: true, mute: false };
 	$('#wrapper').tubular(options);
 	// f-UGhWj1xww cool sepia hd
 	// 49SKbS7Xwf4 beautiful barn sepia

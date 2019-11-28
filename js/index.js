bfb574a6b13d1b9d384338a0de81ba3f5ea8db28
Mattjo <highbrighten@gmail.com>
2019-11-28 09:11:00 +0900

    Test for youtube background

---
+++ js/index.js
@@ -0,0 +1,6 @@
+$('document').ready(function() {
+	var options = { videoId: 'ab0TSkLe-E0', start: 0, repeat: true, mute: ture };
+	$('#wrapper').tubular(options);
+	// f-UGhWj1xww cool sepia hd
+	// 49SKbS7Xwf4 beautiful barn sepia
+});

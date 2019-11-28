c909f8e3db01706325661fb59fc92701103ba388
Mattjo <highbrighten@gmail.com>
2019-11-28 09:21:40 +0900

    수정

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 $('document').ready(function() {
-	var options = { videoId: 'ab0TSkLe-E0', start: 0, repeat: true, mute: ture };
+	var options = { videoId: '49SKbS7Xwf4', start: 0, repeat: true, mute: true };
 	$('#wrapper').tubular(options);
 	// f-UGhWj1xww cool sepia hd
 	// 49SKbS7Xwf4 beautiful barn sepia

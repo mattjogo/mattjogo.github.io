c17e4ba1fa1b1b91a646876e61aef490769a0d3e
Mattjo <highbrighten@gmail.com>
2019-11-29 09:42:13 +0900

    상단 여백 조정 및 index.js 롤백

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 $('document').ready(function() {
 	var options = { videoId: 'e6y5JpPG0Ug', start: 0, repeat: true, mute: false };
-	$('#wrapper').fadeIn(1000).removeClass('hidden');
+	$('#wrapper').tubular(options);
 	// f-UGhWj1xww cool sepia hd
 	// 49SKbS7Xwf4 beautiful barn sepia
 });

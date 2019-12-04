9b3deb06395357ed074962a8d2582ca3d52950b5
Mattjo <highbrighten@gmail.com>
2019-12-04 22:10:58 +0900

    영상 적용 test

--- js/index.js
+++
@@ -1,6 +0,0 @@
-$('document').ready(function() {
-	var options = { videoId: 'e6y5JpPG0Ug', start: 0, repeat: true, mute: false };
-	$('#wrapper').tubular(options);
-	// f-UGhWj1xww cool sepia hd
-	// 49SKbS7Xwf4 beautiful barn sepia
-});
